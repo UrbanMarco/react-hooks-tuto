7a4a16f4d509c486531528d346f48ec48ba55dfb
Marco Ardizzone <marco.ardizzone@urbancampus.com>
2021-08-20 16:32:57 +0200

    useEffect with deps

--- src/components/03_UseEffect/HookCounterOne.js
+++ src/components/03_UseEffect/HookCounterOne.js
@@ -2,15 +2,17 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterOne() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
   useEffect(() => {
+    console.log(`Updating doc title`)
     document.title = `You clicked ${count} times`
-  })
+  }, [count])
 
   return (
     <div>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count+1)}>Click {count} times</button>    
-      <button onClick={() => setCount(0)}>Reset</button>    
     </div>
   )
 }
