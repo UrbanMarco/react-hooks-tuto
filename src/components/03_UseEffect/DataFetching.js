d5a327777802665a12ce5478f0a9443e79693995
Marco Ardizzone <marco.ardizzone@urbancampus.com>
2021-09-10 17:13:34 +0200

    useEffect with button click

--- src/components/03_UseEffect/DataFetching.js
+++ src/components/03_UseEffect/DataFetching.js
@@ -4,9 +4,10 @@ import axios from 'axios'
 function DataFetching() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromBtnClick, setIdFromBtnClick] = useState(1)
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromBtnClick}`)
     .then(res => {
       console.log(res)
       setPost(res.data)
@@ -14,11 +15,16 @@ function DataFetching() {
     .catch(err => {
       console.log(err)
     })
-  }, [id])
+  }, [idFromBtnClick])
+
+  const handleClick = () => {
+    setIdFromBtnClick(id)
+  }
 
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}> Fetch post </button>
       <div>{post.title}</div>
     </div>
   )
