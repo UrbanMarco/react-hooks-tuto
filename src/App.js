f74c86e5c515a48ca1c09fea97c9119dd288f61e
Marco Ardizzone <marco.ardizzone@urbancampus.com>
2021-09-10 17:29:55 +0200

    UseContext example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import './App.css';
-import DataFetching from './components/03_UseEffect/DataFetching';
+import ComponentC from './components/04_UseContext/ComponentC';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'Marco'}>
+        <ChannelContext.Provider value={'React Hooks tuto'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/components/04_UseContext/ComponentF.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ComponentE from './ComponentE'
+
+function ComponentF() {
+  return (
+    <ComponentE />
+  ) 
+}
+
+export default ComponentF
